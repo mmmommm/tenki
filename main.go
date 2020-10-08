0a04dd62e1892df75a78fd16bf42132589df1f70
mmmommm <ryota1839@gmail.com>
2020-10-08 17:27:26 +0900

    ls -aを実装

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
 
 func main()  {
-	fmt.Println("hello world")
+	//カレントディレクトリ取得
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if 2 <= len(os.Args) {
+		if os.Args[1] == "-a" {
+			for _, fileInfo := range fileInfos {
+				fmt.Println(fileInfo.Name())
+			}
+			return
+		}
+	}
+
+	for _, fileInfo := range fileInfos {
+		if strings.HasPrefix(fileInfo.Name(), ".") {
+			continue
+		}
+		fmt.Println(fileInfo.Name())
+	}
 }
