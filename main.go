e55a4adb1e8b4901dd2a03941170d9060c8ed82e
mmmommm <ryota1839@gmail.com>
2020-10-09 21:35:54 +0900

    -cコマンドの動作確認、-hの時の説明を変更

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+
 	"github.com/mmmommm/tenki/tenki"
 )
 
 func main() {
 	const defaultPrefecture = ""
-	var	prefecture string
-	flag.StringVar(&prefecture, "current", defaultPrefecture, "today's weather where you are")
-	flag.StringVar(&prefecture, "c", defaultPrefecture, "today's weather where you are")
-	flag.StringVar(&prefecture, "next", defaultPrefecture, "tommorow's weather where you are")
-	flag.StringVar(&prefecture, "n", defaultPrefecture, "tomorrow's weather where you are")
+	var	cPrefecture, nPrefecture string
+	flag.StringVar(&cPrefecture, "current", defaultPrefecture, "--curennt <県名> returns current weather information where you are.")
+	flag.StringVar(&cPrefecture, "c", defaultPrefecture, "-c <県名> returns weather information where you are.")
+	flag.StringVar(&nPrefecture, "next", defaultPrefecture, "--next <県名> returns tomorrow's weather forecast where you are.")
+	flag.StringVar(&nPrefecture, "n", defaultPrefecture, "-n <県名> returns tomorrow's weather forecast where you are.")
 	flag.Parse()
-	tenki.Current(prefecture)
+	// -cオプションの時の処理を書く
+	if cPrefecture != "" && nPrefecture == "" {
+		tenki.Current(cPrefecture)
+	// -nオプションの時の処理を書く
+	} else if cPrefecture == "" && nPrefecture != "" {
+		fmt.Print("明日の予定を表示します")
+	} else {
+		fmt.Printf("%s: requestType is not correct!\n", os.Args[0])
+		fmt.Printf("%s: try 'tenki --help' or 'tenki -h' for more information\n", os.Args[0])
+		os.Exit(1)
+	}
 }
