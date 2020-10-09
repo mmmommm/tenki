9fc81d9efc871e1ee2da6a33176607444462fc21
mmmommm <ryota1839@gmail.com>
2020-10-09 23:11:34 +0900

    -nコマンドを実装

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	const defaultPrefecture = ""
 	var	cPrefecture, nPrefecture string
 	flag.StringVar(&cPrefecture, "current", defaultPrefecture, "--curennt <県名> returns current weather information where you are.")
-	flag.StringVar(&cPrefecture, "c", defaultPrefecture, "-c <県名> returns weather information where you are.")
+	flag.StringVar(&cPrefecture, "c", defaultPrefecture, "-c <県名> returns current weather information where you are.")
 	flag.StringVar(&nPrefecture, "next", defaultPrefecture, "--next <県名> returns tomorrow's weather forecast where you are.")
 	flag.StringVar(&nPrefecture, "n", defaultPrefecture, "-n <県名> returns tomorrow's weather forecast where you are.")
 	flag.Parse()
@@ -21,9 +21,9 @@ func main() {
 		tenki.Current(cPrefecture)
 	// -nオプションの時の処理を書く
 	} else if cPrefecture == "" && nPrefecture != "" {
-		fmt.Print("明日の予定を表示します")
+		tenki.Next(nPrefecture)
 	} else {
-		fmt.Printf("%s: requestType is not correct!\n", os.Args[0])
+		fmt.Printf("%s: option arguments is empty !! please type prefecture name. \n", os.Args[0])
 		fmt.Printf("%s: try 'tenki --help' or 'tenki -h' for more information\n", os.Args[0])
 		os.Exit(1)
 	}
