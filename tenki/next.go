d4e872bdd4b36c497f17571d97974e08cc21fbff
mmmommm <ryota1839@gmail.com>
2020-10-13 17:28:17 +0900

    時刻が一致する時だけ予報を返す分岐を作成

--- tenki/next.go
+++ tenki/next.go
@@ -43,6 +43,12 @@ type nWind struct {
 	Speed float64 `json:"speed"`
 }
 
+var layout = "2006-01-02 15:04:05"
+func timeToString(t time.Time) string {
+    str := t.Format(layout)
+    return str
+}
+
 func NextWeather(fPrefecture string) {
 	err := godotenv.Load()
   if err != nil {
@@ -78,21 +84,23 @@ func NextWeather(fPrefecture string) {
 			panic(err)
 	}
 
-	//現在時刻を取得する
+	//天気予報が欲しい時間の配列を返す
+	forecastMinutes := round.Tommorow()
 
-	times := [...] int{2, 4, 6}
+	times := [...] int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	for i := range times {
-		fmt.Printf("----------------------------------------\n")
-		fmt.Printf("時刻:     %s\n", time.Unix(apiRes.List[i].Dt, 0))
-		fmt.Printf("天気:     %s\n", apiRes.List[i].Weather[0].Description)
-		fmt.Printf("最高気温: %s °C\n", fmt.Sprintf("%.1f", round.Change(apiRes.List[i].Main.TempMax)))
-		fmt.Printf("最低気温: %s °C\n", fmt.Sprintf("%.1f", round.Change(apiRes.List[i].Main.TempMin)))
-		fmt.Printf("湿度:     %d ％\n", apiRes.List[i].Main.Humidity)
-		fmt.Printf("風速:     %s m/s\n", fmt.Sprintf("%.1f", apiRes.List[i].Wind.Speed))
+		fmt.Print(timeToString(time.Unix(apiRes.List[i].Dt, 0)))
+		for _, v := range forecastMinutes {
+			if timeToString(time.Unix(apiRes.List[i].Dt, 0)) == v {
+				fmt.Printf("----------------------------------------\n")
+				fmt.Printf("時刻:     %s\n", time.Unix(apiRes.List[i].Dt, 0))
+				fmt.Printf("天気:     %s\n", apiRes.List[i].Weather[0].Description)
+				fmt.Printf("最高気温: %s °C\n", fmt.Sprintf("%.1f", round.Change(apiRes.List[i].Main.TempMax)))
+				fmt.Printf("最低気温: %s °C\n", fmt.Sprintf("%.1f", round.Change(apiRes.List[i].Main.TempMin)))
+				fmt.Printf("湿度:     %d ％\n", apiRes.List[i].Main.Humidity)
+				fmt.Printf("風速:     %s m/s\n", fmt.Sprintf("%.1f", apiRes.List[i].Wind.Speed))
+			}
+		}
+		return
 	}
 }
-
-// fmt.Printf("時刻:     %s\n", apiRes.List[0].DtText)
-// fmt.Printf("天気:     %s\n", apiRes.List[0].Weather[0].Main)
-// fmt.Printf("アイコン: https://openweathermap.org/img/wn/%s@2x.png\n", apiRes.List[0].Weather[0].Icon)
-// fmt.Printf("気圧:     %d hPa\n", apiRes.List[0].Main.Pressuer)
