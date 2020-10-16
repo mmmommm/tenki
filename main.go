cfd69e8a1e4320639a6eb89956a467949396fdea
mmmommm <ryota1839@gmail.com>
2020-10-16 12:00:27 +0900

    -vコマンドを追加

--- main.go
+++ main.go
@@ -8,23 +8,24 @@ import (
 	"github.com/mmmommm/tenki/tenki"
 )
 
-var (
-	Version  = "unset"
-	Revision = "unset"
-)
+var version = "1.0.0"
 
 func main() {
 	const defaultPrefecture = ""
 	var cPrefecture, nPrefecture, fPrefecture string
+	var showVersion bool
 	flag.StringVar(&cPrefecture, "current", defaultPrefecture, "--curennt <県名> returns current weather information where you are.")
 	flag.StringVar(&cPrefecture, "c", defaultPrefecture, "-c <県名> returns current weather information where you are.")
 	flag.StringVar(&fPrefecture, "forecast", defaultPrefecture, "--forecast <県名> returns current weather information where you are.")
 	flag.StringVar(&fPrefecture, "f", defaultPrefecture, "-f <県名> returns current weather information where you are.")
 	flag.StringVar(&nPrefecture, "next", defaultPrefecture, "--next <県名> returns tomorrow's weather forecast where you are.")
 	flag.StringVar(&nPrefecture, "n", defaultPrefecture, "-n <県名> returns tomorrow's weather forecast where you are.")
+	flag.BoolVar(&showVersion, "v", false, "show version")
+	flag.BoolVar(&showVersion, "version", false, "show version")
 	flag.Parse()
+
 	// -cオプションの時の処理を書く
-	if cPrefecture != "" && nPrefecture == "" && nPrefecture == "" {
+	if cPrefecture != "" && fPrefecture == "" && nPrefecture == "" {
 		tenki.CurrentWeather(cPrefecture)
 		// -fオプションの時の処理を書く
 	} else if cPrefecture == "" && fPrefecture != "" && nPrefecture == "" {
@@ -32,6 +33,9 @@ func main() {
 		// -nオプションの時の処理を書く
 	} else if cPrefecture == "" && fPrefecture == "" && nPrefecture != "" {
 		tenki.NextWeather(nPrefecture)
+	} else if showVersion {
+		fmt.Println("version:", version)
+		return
 	} else {
 		fmt.Printf("%s: option arguments is empty !! please type prefecture name. \n", os.Args[0])
 		fmt.Printf("%s: try 'tenki --help' or 'tenki -h' for more information\n", os.Args[0])
