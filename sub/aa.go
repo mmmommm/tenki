1d904d2c8e5a548fc27d532246c8122276376020
mmmommm <ryota1839@gmail.com>
2020-10-18 15:40:48 +0900

    roundディレクトリをsubディレクトリに変更

---
+++ sub/aa.go
@@ -0,0 +1,48 @@
+package round
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func readfile(url string) string {
+	file, err := os.Open(url)
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+	lines := string(b)
+	return lines
+}
+
+func ASCIIart(icon string) string {
+	var text string
+	if icon == "01d" || icon == "01n" {
+		text = readfile("./aa/01.txt")
+	} else if icon == "02d" || icon == "02n" {
+		text = readfile("./aa/02.txt")
+	} else if icon == "03d" || icon == "03n" {
+		text = readfile("./aa/03.txt")
+	} else if icon == "04d" || icon == "04n" {
+		text = readfile("./aa/04.txt")
+	} else if icon == "09d" || icon == "09n" {
+		text = readfile("./aa/09.txt")
+	} else if icon == "10d" || icon == "10n" {
+		text = readfile("./aa/10.txt")
+	} else if icon == "11d" || icon == "11n" {
+		text = readfile("./aa/11.txt")
+	} else if icon == "13d" || icon == "13n" {
+		text = readfile("./aa/13.txt")
+	} else if icon == "50d" || icon == "50n" {
+		text = readfile("./aa/50.txt")
+	} else {
+		fmt.Print("-----------------------")
+	}
+	return text
+}
---
+++ sub/time.go
@@ -0,0 +1,28 @@
+package round
+
+import (
+	"time"
+)
+
+var layout = "2006-01-02 15:04:05"
+
+func timeToString(t time.Time) string {
+	str := t.Format(layout)
+	return str
+}
+
+var minutesList = [...]string{" 06:00:00", " 12:00:00", " 15:00:00", " 18:00:00"}
+
+//現在時刻を取得して翌日の6時、12時、15時、18時のデータを表示できるようにする
+func Tommorow() []string {
+	tNow := time.Now()
+	//翌日の同時刻を文字列で取得
+	tTomorrow := timeToString(tNow.AddDate(0, 0, 1))
+	//文字列から年月日だけを抽出
+	picDate := tTomorrow[:10]
+	var forecastMinutes []string
+	for _, v := range minutesList {
+		forecastMinutes = append(forecastMinutes, picDate+v)
+	}
+	return forecastMinutes
+}
